f697ef3dd04dd4027d2ddeaf42f1bdbf90ba664b
Poonam03 <68641541+Poonam03@users.noreply.github.com>
2020-07-22 23:56:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,16 @@
-var ball,img,paddle;
+var ball,ballImg,paddle;
 function preload() {
   /* preload your images here of the ball and the paddle */
+  ballImg=loadImage("ball.png");
 }
 function setup() {
   createCanvas(400, 400);
    /* create the Ball Sprite and the Paddle Sprite */
+   ball=createSprites(350,100,20,20);
   /* assign the images to the sprites */
-  
+  ball=addImage(ballImg);
   /* give the ball an initial velocity of 9 in the X direction */
-  
+  ball.velocityX=-3;
 
 }
 
